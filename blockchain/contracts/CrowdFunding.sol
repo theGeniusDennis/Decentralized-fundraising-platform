c8d7fffda0ca02c29576697fe95490f1f5056aed
theGeniusDennis <danim005@st.ug.edu.gh>
2024-09-15 22:33:37 -0700

    Done with the test cases

--- blockchain/contracts/CrowdFunding.sol
+++ blockchain/contracts/CrowdFunding.sol
@@ -1,9 +1,9 @@
-  // SPDX-License-Identifier: MIT
- pragma solidity ^0.8.24;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
-contract CrowdingFunding{
+contract CrowdingFunding {
   struct Campaign {
-    //struct for the creator
+    // Struct for the creator
     address payable creator;
     uint goal;
     uint amountRaised;
@@ -11,41 +11,40 @@ contract CrowdingFunding{
     bool isCompleted;
   }
 
-  //mapings to keep track of campaigns and [campaign ids to funders and contributions]
+  // Mappings to keep track of campaigns and [campaign ids to funders and contributions]
   mapping(uint => Campaign) public campaigns;
   mapping(uint => mapping(address => uint)) public contributions;
 
-  //Id to keep track of campaigns
+  // ID to keep track of campaigns
   uint public campaignCount;
 
-  //Events
+  // Events
   event campaignCreated(uint campaignId, address creator, uint goal, uint deadline);
   event contributionMade(uint campaignId, address contributor, uint amount);
-  event fundsWithDrawn(uint campaignId, uint amount);
+  event fundsWithdrawn(uint campaignId, uint amount);
 
-  //functionalites
-  //remember to take off the duration
-  function createCampaign(uint _goal, uint _dealine) external {
+  // Functionality
+  function createCampaign(uint _goal, uint _deadline) external {
     require(_goal > 0, "The goal should be greater than zero");
 
     campaignCount++;
 
     campaigns[campaignCount] = Campaign({
-       creator: payable(msg.sender),
-       goal: _goal,
-       amountRaised: 0,
-       deadline: block.timestamp + _dealine,
-       isCompleted: false
+      creator: payable(msg.sender),
+      goal: _goal,
+      amountRaised: 0,
+      deadline: block.timestamp + _deadline,
+      isCompleted: false
     });
 
-    emit campaignCreated(campaignCount, msg.sender, _goal, block.timestamp + _dealine);
+    emit campaignCreated(campaignCount, msg.sender, _goal, block.timestamp + _deadline);
   }
 
   function contribute(uint _campaignId) external payable {
     Campaign storage campaign = campaigns[_campaignId];
 
     require(block.timestamp < campaign.deadline, "Campaign has ended!");
-    require(msg.value > 0, "Contributon should be created than zero");
+    require(msg.value > 0, "Contribution should be greater than zero");
 
     contributions[_campaignId][msg.sender] += msg.value;
     campaign.amountRaised += msg.value;
@@ -53,12 +52,12 @@ contract CrowdingFunding{
     emit contributionMade(_campaignId, msg.sender, msg.value);
   }
 
-  function withdrawFunds(uint _campaignId) external  {
+  function withdrawFunds(uint _campaignId) external {
     Campaign storage campaign = campaigns[_campaignId];
 
-    require(msg.sender == campaign.creator, "Only creator ca withdraw funds");
+    require(msg.sender == campaign.creator, "Only creator can withdraw funds");
     require(block.timestamp > campaign.deadline, "The campaign has not ended!");
-    require(campaign.amountRaised >= campaign.goal);
+    require(campaign.amountRaised >= campaign.goal, "Goal not met");
     require(!campaign.isCompleted, "Funds already withdrawn!");
 
     campaign.isCompleted = true;
@@ -66,15 +65,15 @@ contract CrowdingFunding{
     uint amount = campaign.amountRaised;
     campaign.creator.transfer(amount);
 
-    emit fundsWithDrawn(_campaignId, amount);
-
+    emit fundsWithdrawn(_campaignId, amount);
   }
 
   function getCampaignDetails(uint _campaignId) external view returns (address, uint, uint, uint, bool) {
-        Campaign storage campaign = campaigns[_campaignId];
-        return (campaign.creator, campaign.goal, campaign.amountRaised, campaign.deadline, campaign.isCompleted);
-    }
-  
+    Campaign storage campaign = campaigns[_campaignId];
+    return (campaign.creator, campaign.goal, campaign.amountRaised, campaign.deadline, campaign.isCompleted);
+  }
 
-
+  function getContribution(uint _campaignId, address contributor) public view returns (uint) {
+    return contributions[_campaignId][contributor];
+  }
 }
--- blockchain/test/CrowdFunding.ts
+++ blockchain/test/CrowdFunding.ts
@@ -2,6 +2,7 @@ import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import {expect} from "chai";
 import {ethers} from "hardhat";
 import hre from "hardhat";
+import { CrowdingFunding__factory } from "../typechain-types";
 
 describe("Crowdfunding", function(){
 
@@ -35,7 +36,9 @@ describe("Crowdfunding", function(){
             expect(campaign.isCompleted).to.be.equal(false);
         })
 
-        it("It should accept contributions and keep track of total amount raised", async function(){
+
+
+        it("It should accept contributions", async function(){
             //create a simple campaign
         const goal = ethers.parseEther('10');
         const date = new Date('December 31, 2024, 00:00:00 UTC');
@@ -43,23 +46,102 @@ describe("Crowdfunding", function(){
 
         const {crowdfunding, addr1} = await loadFixture(deployCrowdfunding);
         await crowdfunding.createCampaign(goal, deadline);
+        const contributionAmount = ethers.parseEther("1");
+
+        
+         await expect(crowdfunding.connect(addr1).contribute(1, {value: contributionAmount})).to.emit(crowdfunding,"contributionMade").withArgs(1, addr1.address, contributionAmount);
+      
+
         const campaign = await crowdfunding.campaigns(1);
+        expect(campaign.amountRaised).to.equal(contributionAmount);
 
-
-        //make a contribution
-        const contribution = ethers.parseEther("5");
-        await crowdfunding.connect(addr1).contribute(1, {value: contribution})
-
-        //check contribution and amount raised
-        const addr1Contribution = await crowdfunding.contributions(1, addr1.address);
-
-        expect(addr1Contribution).to.equal(contribution);
-        expect(campaign.amountRaised).to.equal(contribution);
-
-            
         })
-    
-    
-   
+
+        it("should allow creator to withdraw funds", async function(){
+            const goal = ethers.parseEther("2");
+            const date = new Date('Noverber 26, 2024, 00:00:00 UTC');
+            const deadline = Math.floor(date.getTime()/1000);
+
+
+            const {crowdfunding, addr1} = await loadFixture(deployCrowdfunding);
+            await crowdfunding.createCampaign(goal, deadline);
+
+
+            const contributionAmount = ethers.parseEther("5");
+
+            await crowdfunding.connect(addr1).contribute(1, { value: contributionAmount });
+
+            await ethers.provider.send("evm_increaseTime", [deadline + 1]);
+            await ethers.provider.send("evm_mine", []);
+
+            await expect(crowdfunding.withdrawFunds(1))
+            .to.emit(crowdfunding, "fundsWithdrawn")
+            .withArgs(1, contributionAmount);
+
+            const campaign = await crowdfunding.campaigns(1);
+            expect(campaign.isCompleted).to.equal(true);
+        })
+
+        it("should not allow contributions after the deadline", async () => {
+            const goal = ethers.parseEther("5");
+            const deadline = 60; // 60 seconds
+
+
+            const {crowdfunding, addr1} = await loadFixture(deployCrowdfunding);
+            await crowdfunding.createCampaign(goal, deadline);
+        
+            const contributionAmount = ethers.parseEther("1");
+        
+            await ethers.provider.send("evm_increaseTime", [deadline + 1]);
+            await ethers.provider.send("evm_mine", []);
+        
+            await expect(
+              crowdfunding.connect(addr1).contribute(1, { value: contributionAmount })
+            ).to.be.revertedWith("Campaign has ended!");
+          });
+
+
+
+          it("should not allow withdrawals before the campaign ends", async () => {
+            const goal = ethers.parseEther("5");
+            const deadline = 60 * 60 * 24 * 30; // 30 days
+
+            const {crowdfunding, addr1} = await loadFixture(deployCrowdfunding);
+        
+            await crowdfunding.createCampaign(goal, deadline);
+        
+            const contributionAmount = ethers.parseEther("5");
+        
+            await crowdfunding.connect(addr1).contribute(1, { value: contributionAmount });
+        
+            await expect(crowdfunding.withdrawFunds(1)).to.be.revertedWith("The campaign has not ended!");
+          });
+
+
+
+          it("should not allow non-creators to withdraw funds", async () => {
+            const goal = ethers.parseEther("5");
+            const deadline = 60; // 60 seconds
+
+            const {crowdfunding, addr1} = await loadFixture(deployCrowdfunding);
+
+        
+            await crowdfunding.createCampaign(goal, deadline);
+        
+            const contributionAmount = ethers.parseEther("5");
+        
+            await crowdfunding.connect(addr1).contribute(1, { value: contributionAmount });
+        
+            await ethers.provider.send("evm_increaseTime", [deadline + 1]);
+            await ethers.provider.send("evm_mine", []);
+        
+            await expect(crowdfunding.connect(addr1).withdrawFunds(1)).to.be.revertedWith("Only creator can withdraw funds");
+          });
+
+
+
+
+
+        
 
 })
